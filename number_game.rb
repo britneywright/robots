52c720b308f59f936187b9865dbb530d1e4f30e7
Britney Wright <blw06g@gmail.com>
2014-11-03 18:56:58 -0500

    update number game

--- number_game.rb
+++ number_game.rb
@@ -1,13 +1,29 @@
 require 'artoo'
 
-connection :firmata, :adaptor => :firmata, :port => '/dev/cu.usbmodem411' # :port path may vary
+connection :firmata, :adaptor => :firmata, :port => '/dev/cu.usbmodem621' # :port path may vary
 device :board, :driver => :device_info
 device :red_led, :driver => :led, :pin => 13
-device :green_led, :driver => :led, :pin => 13
+device :green_led, :driver => :led, :pin => 12
 
-secret_number = rand(11)
 
 work do
+
+  def play
+    @secret_number = rand(11)
+    puts "Guess what number I'm thinking of, from 0 through 10:"
+    @guess = gets.chomp.to_i
+    guess_sequence
+  end
+
+  def play_again
+    puts "Do you want to play again? Y or N?"
+    response = gets.chomp
+    if response == "Y"
+      play
+    else
+      exit!
+    end
+  end
 
   def blink(led)
     led.on
@@ -15,20 +31,28 @@ work do
     led.off
   end
 
-  puts "Guess what number I'm thinking of:"
-  guess = gets.chomp.to_i
-
-  until secret_number == guess do
-    if guess > secret_number
-      puts "Too high. Guess again."
+  def correct_answer
+    puts "Awesome you got it!"
+    2.times do
       blink(red_led)
-    elsif guess < secret_number
-      puts "Too low. Guess again."
       blink(green_led)
     end
-    guess = gets.chomp.to_i
+    play_again
+  end
+  
+  def guess_sequence
+    until @secret_number == @guess do
+      if @guess > @secret_number
+        puts "Too high. Guess again."
+        blink(red_led)
+      elsif @guess < @secret_number
+        puts "Too low. Guess again."
+        blink(green_led)
+      end
+      @guess = gets.chomp.to_i
+    end  
+    correct_answer
   end
 
-  puts "Awesome you got it!"
-
+  play
 end
